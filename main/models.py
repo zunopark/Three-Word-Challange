340ba286e10d2e5f5e206cd39b263a74b67609cb
zunopark <enzoribery@naver.com>
2020-05-03 14:56:16 +0900

    국가 랭킹 미완성

--- main/models.py
+++ main/models.py
@@ -27,10 +27,205 @@ class Keyword(models.Model):
 # 나라별 검색 및 총 좋아요 랭킹 구현 위해서.
 class Nation(models.Model):
     nation_list = (
-        ('한국', 'Korea'),
-        ('일본', 'Japan'),
-        ('중국', 'China'),
+        ('Afghanistan', '아프가니스탄'),
+        ('Albania', '알바니아'),
+        ('Algeria', '알제리아'),
+        ('Andorra', '안도라')
     )
+# Angola
+# Antigua and Barbuda
+# Argentina
+# Armenia
+# Australia
+# Austria
+# Azerbaijan
+# Bahamas
+# Bahrain
+# Bangladesh
+# Barbados
+# Belarus
+# Belgium
+# Belize
+# Benin
+# Bhutan
+# Bolivia
+# Bosnia and Herzegovina
+# Botswana
+# Brazil
+# Brunei
+# Bulgaria
+# Burkina Faso
+# Burundi
+# Cabo Verde
+# Cambodia
+# Cameroon
+# Canada
+# Central African Republic (CAR)
+# Chad
+# Chile
+# China
+# Colombia
+# Comoros
+# Congo, Democratic Republic of the
+# Congo, Republic of the
+# Costa Rica
+# Cote d'Ivoire
+# Croatia
+# Cuba
+# Cyprus
+# Czechia
+# Denmark
+# Djibouti
+# Dominica
+# Dominican Republic
+# Ecuador
+# Egypt
+# El Salvador
+# Equatorial Guinea
+# Eritrea
+# Estonia
+# Eswatini
+# Ethiopia
+# Fiji
+# Finland
+# France
+# Gabon
+# Gambia
+# Georgia
+# Germany
+# Ghana
+# Greece
+# Grenada
+# Guatemala
+# Guinea
+# Guinea-Bissau
+# Guyana
+# Haiti
+# Honduras
+# Hungary
+# Iceland
+# India
+# Indonesia
+# Iran
+# Iraq
+# Ireland
+# Israel
+# Italy
+# Jamaica
+# Japan
+# Jordan
+# Kazakhstan
+# Kenya
+# Kiribati
+# Kosovo
+# Kuwait
+# Kyrgyzstan
+# Laos
+# Latvia
+# Lebanon
+# Lesotho
+# Liberia
+# Libya
+# Liechtenstein
+# Lithuania
+# Luxembourg
+# Madagascar
+# Malawi
+# Malaysia
+# Maldives
+# Mali
+# Malta
+# Marshall Islands
+# Mauritania
+# Mauritius
+# Mexico
+# Micronesia
+# Moldova
+# Monaco
+# Mongolia
+# Montenegro
+# Morocco
+# Mozambique
+# Myanmar
+# Namibia
+# Nauru
+# Nepal
+# Netherlands
+# New Zealand
+# Nicaragua
+# Niger
+# Nigeria
+# North Korea
+# North Macedonia (formerly Macedonia)
+# Norway
+# Oman
+# Pakistan
+# Palau
+# Palestine
+# Panama
+# Papua New Guinea
+# Paraguay
+# Peru
+# Philippines
+# Poland
+# Portugal
+# Qatar
+# Romania
+# Russia
+# Rwanda
+# Saint Kitts and Nevis
+# Saint Lucia
+# Saint Vincent and the Grenadines
+# Samoa
+# San Marino
+# Sao Tome and Principe
+# Saudi Arabia
+# Senegal
+# Serbia
+# Seychelles
+# Sierra Leone
+# Singapore
+# Slovakia
+# Slovenia
+# Solomon Islands
+# Somalia
+# South Africa
+# South Korea
+# South Sudan
+# Spain
+# Sri Lanka
+# Sudan
+# Suriname
+# Sweden
+# Switzerland
+# Syria
+# Taiwan
+# Tajikistan
+# Tanzania
+# Thailand
+# Timor-Leste
+# Togo
+# Tonga
+# Trinidad and Tobago
+# Tunisia
+# Turkey
+# Turkmenistan
+# Tuvalu
+# Uganda
+# Ukraine
+# United Arab Emirates (UAE)
+# United Kingdom (UK)
+# United States of America (USA)
+# Uruguay
+# Uzbekistan
+# Vanuatu
+# Vatican City (Holy See)
+# Venezuela
+# Vietnam
+# Yemen
+# Zambia
+# Zimbabwe
+
     objects = models.Manager()
     name = models.CharField(max_length=100, choices=nation_list)
 
